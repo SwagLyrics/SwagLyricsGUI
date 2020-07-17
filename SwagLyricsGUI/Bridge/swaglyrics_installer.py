0b4df4701a71736dc221932e421309e2c3f924c9
flabbet <flubbet@gmail.com>
2020-07-17 21:05:37 +0200

    Changed csproj and version

--- SwagLyricsGUI/Bridge/swaglyrics_installer.py
+++ SwagLyricsGUI/Bridge/swaglyrics_installer.py
@@ -6,6 +6,8 @@ required = {'swaglyrics'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
+print("Launching...")
+
 if missing:
     python = sys.executable
     print("Installing swaglyrics...")
